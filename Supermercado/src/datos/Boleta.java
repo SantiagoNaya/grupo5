d27923a5ed90f39015b1d2576d1f739c2cf3de1d
santiago <54956952+SantiagoNaya@users.noreply.github.com>
2022-10-05 17:35:30 -0300

    cambio cree constructor

--- Supermercado/src/datos/Boleta.java
+++ Supermercado/src/datos/Boleta.java
@@ -6,4 +6,11 @@ public class Boleta {
 private int id_boleta;
 private String contenido;
 private Date fecha;
+public Boleta(int id_boleta, String contenido, Date fecha) {
+	super();
+	this.id_boleta = id_boleta;
+	this.contenido = contenido;
+	this.fecha = fecha;
+}
+
 }
