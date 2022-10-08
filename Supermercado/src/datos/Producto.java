408bd15e9df4a0397d6ed0410f827cc093d08b1e
Felipe Lacourt <felipelacourt1@gmail.com>
2022-10-08 22:58:50 +0200

    Finalización de Datos

--- Supermercado/src/datos/Producto.java
+++ Supermercado/src/datos/Producto.java
@@ -5,4 +5,42 @@ private String articulo;
 private int cantidad;
 private int id_stock;
 
+public Producto(String articulo, int cantidad, int id_stock) {
+	super();
+	this.articulo = articulo;
+	this.cantidad = cantidad;
+	this.id_stock = id_stock;
+}
+
+public String getArticulo() {
+	return articulo;
+}
+
+public void setArticulo(String articulo) {
+	this.articulo = articulo;
+}
+
+public int getCantidad() {
+	return cantidad;
+}
+
+public void setCantidad(int cantidad) {
+	this.cantidad = cantidad;
+}
+
+public int getId_stock() {
+	return id_stock;
+}
+
+public void setId_stock(int id_stock) {
+	this.id_stock = id_stock;
+}
+
+@Override
+public String toString() {
+	return "Producto [articulo=" + articulo + ", cantidad=" + cantidad + ", id_stock=" + id_stock + "]";
+}
+
+
+
 }
