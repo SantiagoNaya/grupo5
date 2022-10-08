c40cd248d1e37b9a29e51ada5498f732f192ea20
santiago <54956952+SantiagoNaya@users.noreply.github.com>
2022-10-08 17:24:57 -0300

    gerente operario
    creacion de constructores getters and setters

--- Supermercado/src/datos/Gerente.java
+++ Supermercado/src/datos/Gerente.java
@@ -3,6 +3,19 @@ package datos;
 public class Gerente extends Usuario {
 private int id_gerente;
 
+@Override
+public String toString() {
+	return "Gerente [id_gerente=" + id_gerente + "]";
+}
+
+public int getId_gerente() {
+	return id_gerente;
+}
+
+public void setId_gerente(int id_gerente) {
+	this.id_gerente = id_gerente;
+}
+
 public Gerente(String nombre, int password, int dni, int nivel, int id_gerente) {
 	super(nombre, password, dni, nivel);
 	this.id_gerente = id_gerente;
--- Supermercado/src/datos/Operario.java
+++ Supermercado/src/datos/Operario.java
@@ -3,6 +3,19 @@ package datos;
 public class Operario extends Usuario  {
 private int id_operario;
 
+public int getId_operario() {
+	return id_operario;
+}
+
+@Override
+public String toString() {
+	return "Operario [id_operario=" + id_operario + "]";
+}
+
+public void setId_operario(int id_operario) {
+	this.id_operario = id_operario;
+}
+
 public Operario(String nombre, int password, int dni, int nivel, int id_operario) {
 	super(nombre, password, dni, nivel);
 	this.id_operario = id_operario;
