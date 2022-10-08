239bfc85fa52a2cdc738431c437765c970e75738
Felipe Lacourt <felipelacourt1@gmail.com>
2022-10-08 22:59:14 +0200

    Finalización de datos.

--- Supermercado/src/datos/Boleta.java
+++ Supermercado/src/datos/Boleta.java
@@ -6,12 +6,43 @@ public class Boleta {
 private int id_boleta;
 private String contenido;
 private Date fecha;
+
 public Boleta(int id_boleta, String contenido, Date fecha) {
 	super();
 	this.id_boleta = id_boleta;
 	this.contenido = contenido;
 	this.fecha = fecha;
-	//s
+	
 }
+
+public int getId_boleta() {
+	return id_boleta;
+}
+
+public void setId_boleta(int id_boleta) {
+	this.id_boleta = id_boleta;
+}
+
+public String getContenido() {
+	return contenido;
+}
+
+public void setContenido(String contenido) {
+	this.contenido = contenido;
+}
+
+public Date getFecha() {
+	return fecha;
+}
+
+public void setFecha(Date fecha) {
+	this.fecha = fecha;
+}
+
+@Override
+public String toString() {
+	return "Boleta [id_boleta=" + id_boleta + ", contenido=" + contenido + ", fecha=" + fecha + "]";
+}
+
 
 }
