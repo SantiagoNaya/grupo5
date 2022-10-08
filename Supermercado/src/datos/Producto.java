2a97d0caa0e747ae29133829d0ff0b732f331248
Alejandro <yoomin7@hotmail.com>
2022-10-08 17:52:57 -0300

    creacion constructores class producto

--- Supermercado/src/datos/Producto.java
+++ Supermercado/src/datos/Producto.java
@@ -5,4 +5,11 @@ private String articulo;
 private int cantidad;
 private int id_stock;
 
+public Producto(String articulo, int cantidad, int id_stock) {
+	super();
+	this.articulo = articulo;
+	this.cantidad = cantidad;
+	this.id_stock = id_stock;
+}
+
 }
