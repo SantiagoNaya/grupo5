f1d98a9c43c0d39f7edf7e2e3aad57020b479072
Felipe Lacourt <felipelacourt1@gmail.com>
2022-10-28 22:09:53 +0200

    Cambios 2.0

--- Supermercado/src/UI/Interfaz.java
+++ Supermercado/src/UI/Interfaz.java
@@ -221,49 +221,18 @@ private void CrearUsuario() {
 	}   */
 	
 	
-	private void imprimirMenu() {
-		
-		
-		
-		int opcion = Integer.parseInt(JOptionPane.showInputDialog("Por favor escoja una de los siguientes niveles:\n"
-				 + "1.Sucursal\n" + "2.SectorVenta\n"+ 
-				"3.Operario\n" + "4.Gerente\n" + "5.Salir"));
-		switch (opcion) {
-		case 1: 
-			
-			menuAlter();
-			//Sucursal();
-			break;
-		case 2:
-			
-			//SectorVenta();
-			break;
-		case 3: 
-			//Operario();
-			break;
-		case 4:
-			//Gerente();
-			break;
-		case 5:
-			   Salir();
-		break;
-			default: JOptionPane.showMessageDialog(null,"Se eligio una opción incorrecta volver a intentar");
-				break;
-			}
-	}
+	
 	
 	private void menuAlter() {
 		
 		
-		
-		
 		int opcion = Integer.parseInt(JOptionPane.showInputDialog("Para acceder a la opción escriba el numero que corresponda:\n"+
 				"1.Crear pedido\n"+ "2.Ver productos\n"+ "3.Salir"));
 		switch (opcion) {
-		case 1: Crear();
+		case 1:
 			break;
 		case 2: 
-			   verPerfiles();
+			   
 			break;
 		case 3: 
 			Salir();
@@ -273,85 +242,10 @@ private void CrearUsuario() {
 			}
 	}
 	
-	private void Crear() {
-
-		
 	
-				
-		Boleta boleta = new Boleta();
-		boleta.setFecha(JOptionPane.showInputDialog("Ingrese la fecha de envio:"));
-		
-		
-		
-		int flag =0;
-		
-		do {
-		//Podes utilizar los array de string para mostrar la lista de productos y queda un poco mas lindo y mostras las opciones directamtne 
-		String[] Producto = {"Queso", "Pan", "Coca cola", "Papas fritas", "Patys", "Helado", "Salir"};
-		
-		Object selection = JOptionPane.showInputDialog(null,"Elija un producto:",
-				"Seleccion",JOptionPane.QUESTION_MESSAGE,null,Producto,null);
-		
-		if(selection.equals("Salir")) {
-			flag =1;
-		}else{
-			JOptionPane.showMessageDialog(null, selection);
-		 String cantidad = JOptionPane.showInputDialog(null, "Ingrese cantidad:");
-		 
-		
-		 boleta.setContenido(selection);
-		 boleta.setCantidad(cantidad);
-		 
-		 if(Productos.add(boleta)) {
-			
-			 int opcion = Integer.parseInt(JOptionPane.showInputDialog("Para acceder a la opción escriba el numero que corresponda:\n"+
-						"1.Crear otro Pedido\n" + "2.Salir"));
-				switch (opcion) {
-				case 1: Crear();
-					break;
-				case 2: Salir();
-				break;
-				}
-		 }else {
-				JOptionPane.showMessageDialog(null, "Error del sistema.");
-		 }
-		} 
-		
-		
-		}while(flag !=1);
-		
-		
-		
-		
-	}
 
-	private String String(Object showInputDialog) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 	
-	private void verPerfiles() {
-		
-		String[] Producto = {"Listado de productos: \n","Queso\n", "Pan\n", "Coca cola\n", "Papas fritas\n", "Patys\n", "Helado"};
-		
-		JOptionPane.showMessageDialog(null, Producto);
-		
-		int opcion = Integer.parseInt(JOptionPane.showInputDialog("Para acceder a la opción escriba el numero que corresponda:\n"+
-				"1.Crear pedido\n"+ "2.Ver productos\n"+ "3.Salir"));
-		switch (opcion) {
-		case 1: Crear();
-			break;
-		case 2: 
-			verPerfiles();
-			break;
-		case 3: 
-			Salir();
-			break;
-			default: JOptionPane.showMessageDialog(null,"Se eligio una opción incorrecta volver a intentar");
-				break;
-			}
-		
-	}
+	
 	
 	private void Salir() {
 		JOptionPane.showMessageDialog(null,"¡Hasta luego!");
